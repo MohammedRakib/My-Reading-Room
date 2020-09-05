680cd459ff3818016867e375486abfb1f3067792
Mohammed Rakib <rafz.rakib@gmail.com>
2020-09-06 02:11:14 +0600

    Completed basic upload with further modifications required

--- Reading_Room-project/Reading_Room/settings.py
+++ Reading_Room-project/Reading_Room/settings.py
@@ -141,3 +141,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
--- Reading_Room-project/create_join_class/forms.py
+++ Reading_Room-project/create_join_class/forms.py
@@ -1,8 +1,18 @@
 from django.forms import ModelForm
-from .models import ClassRoom
+from .models import ClassRoom, ReadingMaterial
 
 
 class CreateClassRoomForm(ModelForm):
     class Meta:
         model = ClassRoom
         fields = ['name', 'section']
+
+class ReadingMaterialForm(ModelForm):
+
+    # def __init__(self, user, *args, **kwargs):
+    #     super(ReadingMaterialForm, self).__init__(*args, **kwargs)
+    #     self.fields['classroom'].queryset = ClassRoom.objects.filter(teacher=user)
+
+    class Meta:
+        model = ReadingMaterial
+        fields = ['name', 'readingFile']
